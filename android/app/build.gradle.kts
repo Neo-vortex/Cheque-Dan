32c5f0da8e18e8dd9f7a461f796e7960804b61f6
Mohamadreza Nakhleh <neo.vortex@pm.me>
2026-05-25 16:29:15 +0330

    fix: increase min sdk to 23

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     ndkVersion = "27.0.12077973"
     namespace = "ir.neovortex.cheque_dan"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
@@ -26,7 +26,8 @@ android {
         applicationId = "ir.neovortex.cheque_dan"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        //minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
